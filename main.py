954659fa12003a20b71e2ec1d590b68df777b3a7
pete.callaghan <pete.callaghan@restartconsulting.com>
2017-06-28 13:56:05 +0100

    Moved the creation of the next generation into the Generation model and created a main method to run the simulation fully

---
+++ main.py
@@ -0,0 +1,31 @@
+from models.population import Population
+from models.config import Config
+from models.epoch import Epochs
+from models.generation import Generations
+
+DEFAULT_KILL_CONSTANT = 1.0255
+
+def main():
+    config = Config()
+    population = Population(config)
+    generations = Generations(population)
+
+    epochs = Epochs()
+    epochs.addNextEpoch(-50, 1, 1, 4000)
+    epochs.addNextEpoch(400, 1.5, DEFAULT_KILL_CONSTANT)
+    epochs.addNextEpoch(550, 1, DEFAULT_KILL_CONSTANT, 2000)
+    epochs.addNextEpoch(1086, 1.1, DEFAULT_KILL_CONSTANT)
+    epochs.addNextEpoch(1300, 1.1, DEFAULT_KILL_CONSTANT)
+    epochs.addNextEpoch(1348, 3, DEFAULT_KILL_CONSTANT)
+    epochs.addNextEpoch(1400, 1, DEFAULT_KILL_CONSTANT)
+    epochs.addNextEpoch(2016, 3, DEFAULT_KILL_CONSTANT)
+    epochs.addNextEpoch(2068, 1, DEFAULT_KILL_CONSTANT)
+    epochs.setFinalEpochYear(-50 + config.number_of_years - 1)
+
+    # When the simulation runs through the epochs
+    for epoch in epochs.epochs:
+        for year in epoch.getRangeOfYears():
+            generation = generations.addNextGeneration(year, epoch)
+
+if __name__ == "__main__":
+    main()
--- models/generation.py
+++ models/generation.py
@@ -1,13 +1,51 @@
-class Generation:
-    def __init__(self, epoch, year, number_born, number_killed):
+from models.population import Population
+from models.config import Config
+from models.epoch import Epoch
+
+import timeit
+
+UNDEFINED_YEAR = -1
+
+class GenerationHistory:
+    def __init__(self, epoch, year, population, number_born, number_killed, born_time, kill_time):
         self.number_born = number_born
         self.number_killed = number_killed
+        self.population = population
         self.year = year
         self.epoch = epoch
+        self.born_time = born_time
+        self.kill_time = kill_time
+
+    def bornElapsedInHundredths(self):
+        return int(self.born_time * 100) / 100
+
+    def killElapsedInHundredths(self):
+        return int(self.kill_time * 100) / 100
 
 class Generations:
-    def __init__(self):
-        self.generations = []
+    def __init__(self, population):
+        self.history = []
+        self.population = population
+        self.first_year = UNDEFINED_YEAR
 
-    def add(self, epoch, year, number_born, number_killed):
-        self.generations.append(Generation(epoch, year, number_born, number_killed))
+    def addNextGeneration(self, year, epoch):
+        if (self.first_year == UNDEFINED_YEAR):
+            self.first_year = year
+            # Add an initial population
+            self.population.addNewIndividuals(year)
+
+        start_time = timeit.default_timer()
+        fatalities = self.population.killThoseUnfitOrReadyToDie(year, epoch)
+        kill_elapsed = timeit.default_timer() - start_time
+
+        start_time = timeit.default_timer()
+        babies = self.population.addNewGeneration(year)
+        born_elapsed = timeit.default_timer() - start_time
+
+        return self.addHistory(epoch, year, len(self.population.individuals), len(babies), len(fatalities), born_elapsed, kill_elapsed)
+
+    def addHistory(self, epoch, year, population, number_born, number_killed, born_time, kill_time):
+        generation = GenerationHistory(epoch, year, population, number_born, number_killed, born_time, kill_time)
+        self.history.append(generation)
+        print('Year', generation.year, 'Pop', generation.population, 'Born', generation.number_born, 'in', generation.bornElapsedInHundredths(), 's Killed', generation.number_killed, 'in', generation.killElapsedInHundredths(), 's')
+        return generation
